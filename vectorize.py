fd685d692b570b6ba4f8b655116324f3330ba268
First Last <abiralbhattarai101@gmail.com>
2025-06-21 13:58:21 +0545

    removed unused modules

--- vectorize.py
+++ vectorize.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
-from langchain_community.document_loaders import TextLoader,PyPDFLoader,JSONLoader
+from langchain_community.document_loaders import TextLoader,PyPDFLoader
 
 load_dotenv()
 
